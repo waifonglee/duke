71651d950bedff9738e5aa12fbfc54ede5258a52
waifonglee <waifonglee_@hotmail.com>
2019-08-20 01:06:33 +0800

    Level-5 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,16 +19,20 @@ public class Duke {
     public static void run() {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
-            String input = sc.nextLine();
-            printOutput(input);
-            if (input.equalsIgnoreCase("bye")) {
-                break;
+            try {
+                String input = sc.nextLine();
+                printOutput(input);
+                if (input.equalsIgnoreCase("bye")) {
+                    break;
+                }
+            } catch (DukeException ex) {
+                System.err.println(ex.getMessage());
             }
         }
     }
 
-    public static void printOutput(String input) {
-        switch (input.toLowerCase().split(" ")[0]) {
+    public static void printOutput(String input) throws DukeException {
+        switch (input.toLowerCase()) {
             case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
@@ -38,40 +42,54 @@ public class Duke {
                     System.out.println(i + 1 + ". " + taskList.get(i));
                 }
                 break;
-            case "done":
-                String[] splitBySpace = input.split(" ");
-                Task current = taskList.get(Integer.parseInt(splitBySpace[1]) - 1);
-                current.markAsDone();
-                System.out.println("Nice! I've marked this task as done: \n" + current);
-                break;
-            case "todo":
-            case "deadline":
-            case "event":
-                Task task = getTask(input);
-                taskList.add(task);
-                System.out.println("Got it. I've added this task: \n" + task + "\nNow you have "
-                        + taskList.size() + " task(s) in the list.");
-                break;
             default:
-                System.out.println("Invalid input");
+                String[] splitBySpace = input.split(" ");
+                if (splitBySpace.length == 2 && splitBySpace[0].equalsIgnoreCase("done")) {
+                    Task current = taskList.get(Integer.parseInt(splitBySpace[1]) - 1);
+                    current.markAsDone();
+                    System.out.println("Nice! I've marked this task as done: \n" + current);
+                } else {
+                    Task task = getTask(input);
+                    taskList.add(task);
+                    System.out.println("Got it. I've added this task: \n" + task + "\nNow you have "
+                            + taskList.size() + " task(s) in the list.");
+                }
         }
     }
 
-    public static Task getTask(String input) {
+    public static Task getTask(String input) throws DukeException {
         String typeOfTask = input.split(" ")[0];
-        String taskDes = input.split(typeOfTask)[1];
+        String[] descArr = input.split(typeOfTask);
         Task newTask;
         switch (typeOfTask.toLowerCase()) {
             case "deadline":
-                String[] splitDl = taskDes.split("/by");
+                if (descArr.length == 0 || descArr[1].trim().isEmpty()) {
+                    throw new DukeException("☹ OOPS!!! The description of deadline cannot be empty.");
+                }
+                String[] splitDl = descArr[1].split("/by");
+                if (splitDl.length < 2 || splitDl[0].trim().isEmpty() || splitDl[1].trim().isEmpty()) {
+                    throw new DukeException("☹ OOPS!!! Wrong format of description.");
+                }
                 newTask = new Deadline(splitDl[0].trim(), splitDl[1].trim());
                 break;
             case "event":
-                String[] splitEv = taskDes.split("/at");
+                if (descArr.length == 0 || descArr[1].trim().isEmpty()) {
+                    throw new DukeException("☹ OOPS!!! The description of this event cannot be empty.");
+                }
+                String[] splitEv = descArr[1].split("/at");
+                if (splitEv.length < 2 || splitEv[0].trim().isEmpty() || splitEv[1].trim().isEmpty()) {
+                    throw new DukeException("☹ OOPS!!! Wrong format of description.");
+                }
                 newTask = new Event(splitEv[0].trim(), splitEv[1].trim());
                 break;
+            case "todo":
+                if (descArr.length == 0 || descArr[1].trim().isEmpty()) {
+                    throw new DukeException("☹ OOPS!!! The description of todo cannot be empty.");
+                }
+                newTask = new Todo(descArr[1].trim());
+                break;
             default:
-                newTask = new Todo(taskDes.trim());
+                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
         return newTask;
     }
