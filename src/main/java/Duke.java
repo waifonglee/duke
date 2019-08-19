efb98135b6e69bd836ba9719d684f377c742ddc9
waifonglee <waifonglee_@hotmail.com>
2019-08-19 20:05:23 +0800

    Level-2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    protected static ArrayList<String> taskList = new ArrayList<String>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,12 +20,26 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
+            printOutput(input);
             if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
+    }
+
+    public static void printOutput(String input) {
+        switch (input.toLowerCase()) {
+            case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                System.out.println(input);
-            }
+            case "list":
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println(i + 1 + ". " + taskList.get(i));
+                }
+                break;
+            default:
+                taskList.add(input);
+                System.out.println("added: " + input);
         }
     }
 }
